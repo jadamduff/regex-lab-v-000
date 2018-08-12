9963b6e791a51a50f72a21e02ddeecbbd6643928
Adam Duff <j.adam.duff@gmail.com>
2018-08-12 06:05:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,11 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/^[A-Z].*[?.!]/) == text
-    return true
-  else
-    return false
-  end
+  text.match(/^[A-Z].*[?.!]/)
 end
 
 def valid_phone_number?(phone)
